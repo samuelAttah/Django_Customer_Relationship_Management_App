e15b478d3a08e140caa8a1f363088569c7f31e7a
Samuel Uchechukwu Attah <87594201+samuelAttah@users.noreply.github.com>
2022-03-05 09:02:59 +0100

    REMOVE SECRET KEY

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n6n+_7q%%_fev9k5ej*#l7&&)1@op#nb2q#yfw*#qb$hrk=gk8'
+# Secret Key was deleted
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
